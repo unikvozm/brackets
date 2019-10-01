944b670e4a7619933e83bc060ee251f607c05847
Natalia Volkova <natalia.volkofff@gmail.com>
2019-10-01 23:28:24 +0500

    feat: solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  //Special case with no brackets
+
+  let bracketsPairs = bracketsConfig.length;
+
+  // storing all types of opening and closing brackets
+  let openings = [];
+  let closings = [];
+  let same = [];
+  for (let i = 0; i < bracketsPairs; i++) {
+    openings.push(bracketsConfig[i][0]);
+    closings.push(bracketsConfig[i][1]);
+    if (bracketsConfig[i][0] === bracketsConfig[i][1]) {
+      same.push(bracketsConfig[i][0]);
+    }
+  }
+
+  // iterate through the string
+  let bracketsTotal = str.length;
+  let allOpened = [];
+  for (let j = 0; j < bracketsTotal; j++) {
+    if (same.includes(str[j])) {
+      if (allOpened[allOpened.length - 1] === same[same.indexOf(str[j])]) {
+        allOpened.pop();
+      }
+      else {
+        allOpened.push(str[j]);
+      }
+    }
+    else if (openings.includes(str[j])) {
+      allOpened.push(str[j]);
+    }
+    else {
+      if (!closings.includes(str[j])) return true;
+      // if it is a pair with the last opened bracket
+      else if (str[j] === closings[openings.indexOf(allOpened[allOpened.length - 1])]) {
+        allOpened.pop();
+      }
+      else return false;
+    }
+  }
+  return allOpened.length === 0 ? true : false; 
 }
